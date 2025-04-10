5e8dc5de003b7b9a15d1101e47907c13536cd24b
nikojpapa <npapa1994@gmail.com>
2025-04-09 18:38:06 -0600

    second identity and zeros

--- tests/error_correcting_codes/generic_stabilizer_code/matrix_standardizer/test_check_matrix_standardizer.py
+++ tests/error_correcting_codes/generic_stabilizer_code/matrix_standardizer/test_check_matrix_standardizer.py
@@ -25,19 +25,36 @@ class CheckMatrixStandardizer:
             qubit_order=self._new_check_matrix.qubit_order,
         )
 
-    def _set_first_identity_matrix(self):
-        for i in range(self._check_matrix.rank_of_pauli_x_portion):
-            if not self._check_matrix.matrix[i, i]:
+    def _set_first_identity_matrix(self) -> None:
+        for i in range(self._new_check_matrix.rank_of_pauli_x_portion):
+            self._set_element_equal_to_one(row_index=i, column_index=i)
+            self._set_rest_of_column_to_zero(row_index_in_identity_form=i, column_index=i)
+
+    def _set_second_identity_matrix(self) -> None:
+        for i in range(self._new_check_matrix.rank_of_pauli_x_portion):
+            row_index = self._new_check_matrix.rank_of_pauli_x_portion + i
+            column_index = self._new_check_matrix.num_physical_qubits + self._new_check_matrix.rank_of_pauli_x_portion + i
+            self._set_element_equal_to_one(row_index=row_index, column_index=column_index)
+            self._set_rest_of_column_to_zero(row_index_in_identity_form=row_index, column_index=column_index)
+
+
+    def _set_element_equal_to_one(self, row_index: int, column_index: int) -> None:
+            if not self._check_matrix.matrix[row_index, column_index]:
                 row_index_with_one_in_column = NextRowIndexWithOneAtPositionFinder(matrix=self._new_check_matrix.matrix,
-                                                                                   row_index=i,
-                                                                                   column_index=i).get_row_index()
+                                                                                   row_index=row_index,
+                                                                                   column_index=column_index).get_row_index()
                 if row_index_with_one_in_column is not None:
-                    self._new_check_matrix.add_rows(row_index=row_index_with_one_in_column, target_row_index=i)
+                    self._new_check_matrix.add_rows(row_index=row_index_with_one_in_column, target_row_index=row_index)
                 else:
                     column_index_with_one_in_row = NextColumnIndexWithOneAtPositionFinder(matrix=self._new_check_matrix.matrix,
-                                                                                          row_index=i,
-                                                                                          column_index=i).get_column_index()
-                    self._new_check_matrix.swap_qubits(column_indices=(i, column_index_with_one_in_row))
+                                                                                          row_index=row_index,
+                                                                                          column_index=column_index).get_column_index()
+                    self._new_check_matrix.swap_qubits(column_indices=(column_index, column_index_with_one_in_row))
+
+    def _set_rest_of_column_to_zero(self, row_index_in_identity_form: int, column_index: int) -> None:
+        for i in range(len(self._new_check_matrix.matrix)):
+            if i != row_index_in_identity_form and self._new_check_matrix.matrix[row_index_in_identity_form, column_index]:
+                self._new_check_matrix.add_rows(row_index=row_index_in_identity_form, target_row_index=i)
 
 
 
