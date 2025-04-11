a82068d90bada5e7f7b7fb7556486ab4158de49c
nikojpapa <npapa1994@gmail.com>
2025-04-11 14:57:40 -0600

    fix generic steane test

--- tests/error_correcting_codes/generic_stabilizer_code/test_logical_state_encoding_general_stabilizer.py
+++ tests/error_correcting_codes/generic_stabilizer_code/test_logical_state_encoding_general_stabilizer.py
@@ -5,15 +5,16 @@ from stim_experiments.error_correcting_codes.generic_stabilizer_code.generic_sta
 from stim_experiments.utilities import KET_ONE_DENSITY_MATRIX, KET_ZERO_DENSITY_MATRIX
 from tests.error_correcting_codes.generic_stabilizer_code.expected_states_generic_5_qubit import \
     ExpectedStatesGenericFiveQubit
+from tests.error_correcting_codes.generic_stabilizer_code.expected_states_generic_steane import \
+    ExpectedStatesGenericSteane
 from tests.error_correcting_codes.generic_stabilizer_code.utilities import get_check_matrix_values_4_qubit, \
     get_check_matrix_values_5_qubit, \
     get_check_matrix_values_steane
-from tests.error_correcting_codes.steane_code.expected_states_steane import ExpectedStatesSteane
 
 
 class TestGenericStabilizerCodeGeneralStabilizer:
     def test_logical_zero_steane(self):
-        expected_state = ExpectedStatesSteane().get_logical_zero_density_matrix()
+        expected_state = ExpectedStatesGenericSteane().get_logical_zero_density_matrix()
         code = GenericStabilizerCode(generators=get_check_matrix_values_steane())
         current_state = code.get_current_state()
         assert allclose(current_state, expected_state, atol=1e-7)
