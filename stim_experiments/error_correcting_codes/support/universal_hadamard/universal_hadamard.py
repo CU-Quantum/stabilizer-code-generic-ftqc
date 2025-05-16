8979809a9753e14dc155e80b672990050e5eeca5
nikojpapa <npapa1994@gmail.com>
2025-05-15 23:25:30 -0600

    organize

---
+++ stim_experiments/error_correcting_codes/support/universal_hadamard/universal_hadamard.py
@@ -0,0 +1,51 @@
+from uuid import uuid4
+
+from cirq import Circuit, CircuitOperation, KeyCondition, MeasurementKey, R, SWAP
+
+from stim_experiments.custom_dataclasses.logical_operation import LogicalGateLabel, LogicalOperation
+from stim_experiments.error_correcting_codes.error_correcting_code.error_correcting_code import ErrorCorrectingCode
+from stim_experiments.error_correcting_codes.support.controlled_single_qubit_gates_applier import \
+    ControlledSingleQubitGatesApplier
+from stim_experiments.error_correcting_codes.support.universal_hadamard.universal_hadamard_code_to_computational_logical import \
+    UniversalHadamardCodeToComputationalLogical
+from stim_experiments.error_correcting_codes.three_cat_code.three_cat_code import ThreeCatCode
+from stim_experiments.error_correcting_codes.universal_hadamard_code.universal_hadamard_code import \
+    UniversalHadamardCode
+from stim_experiments.singletons.error_correcting_code_configuration import ConfigurationErrorCorrectingCodeManager
+from stim_experiments.singletons.fresh_ancillas_pool import FreshAncillasPool
+
+
+class UniversalHadamard:
+    def __init__(self, code: ErrorCorrectingCode = None):
+        self._code = code
+
+        configuration = ConfigurationErrorCorrectingCodeManager().get_configuration()
+        self._measurer_type = configuration.measurer_type
+
+    def get_hadamard_circuit(self) -> Circuit:
+        measurement_key = MeasurementKey(f'UNIVERSAL_HADAMARD_MEASUREMENT_{uuid4().hex}')
+        num_qubits_in_desired_encoding = len(self._code.data_qubits)
+        with FreshAncillasPool().use_fresh_ancillas(num_ancillas=3 * num_qubits_in_desired_encoding) as ancilla_qubits:
+            three_cat = ThreeCatCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=ancilla_qubits)
+            uni_h = UniversalHadamardCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=ancilla_qubits)
+            new_encoding = self._code.create_new(qubits=uni_h.subregisters[0])
+            to_computational_logical = UniversalHadamardCodeToComputationalLogical(universal_hadamard_code=uni_h, desired_encoding=self._code)
+            logical_x, logical_z = (list(self._code.get_operation_circuit(operation=LogicalOperation(gate=gate, qubit_index=0)).all_operations())  # allow multi qubit encodings
+                                    for gate in (LogicalGateLabel.X, LogicalGateLabel.Z))
+            return Circuit(
+                three_cat.encode_logical_qubit(),
+                [
+                    ControlledSingleQubitGatesApplier(operations=logical_x, controls=subregister[:len(logical_x)]).get_circuit()
+                    for subregister in three_cat.subregisters
+                ],
+                to_computational_logical.get_circuit(),
+                self._measurer_type(operations=logical_z, measurement_key=measurement_key).get_measurement_circuit(),
+                CircuitOperation(
+                    new_encoding.get_operation_circuit(LogicalOperation(gate=LogicalGateLabel.Z, qubit_index=0)).freeze()
+                ).with_classical_controls(KeyCondition(key=measurement_key)),
+                [
+                    SWAP(new_encoding.data_qubits[i], self._code.data_qubits[i])
+                    for i in range(len(new_encoding.data_qubits))
+                ],
+                [R(ancilla) for ancilla in ancilla_qubits]
+            )
--- tests/error_correcting_codes/support/universal_hadamard/test_universal_hadamard.py
+++ tests/error_correcting_codes/support/universal_hadamard/test_universal_hadamard.py
@@ -1,60 +1,12 @@
-from uuid import uuid4
-
 import numpy as np
-from cirq import Circuit, CircuitOperation, KeyCondition, LineQubit, MeasurementKey, R, SWAP, Z
+from cirq import Circuit, LineQubit
 from numpy import array
 
-from stim_experiments.custom_dataclasses.logical_operation import LogicalGateLabel, LogicalOperation
-from stim_experiments.error_correcting_codes.error_correcting_code.error_correcting_code import ErrorCorrectingCode
 from stim_experiments.error_correcting_codes.error_correcting_code_utilities import get_error_correcting_code_utilities
-from stim_experiments.error_correcting_codes.support.controlled_single_qubit_gates_applier import \
-    ControlledSingleQubitGatesApplier
-from stim_experiments.error_correcting_codes.support.universal_hadamard.universal_hadamard_code_to_computational_logical import \
-    UniversalHadamardCodeToComputationalLogical
-from stim_experiments.error_correcting_codes.three_cat_code.three_cat_code import ThreeCatCode
-from stim_experiments.error_correcting_codes.universal_hadamard_code.universal_hadamard_code import \
-    UniversalHadamardCode
-from stim_experiments.singletons.error_correcting_code_configuration import ConfigurationErrorCorrectingCodeManager
+from stim_experiments.error_correcting_codes.support.universal_hadamard.universal_hadamard import UniversalHadamard
 from stim_experiments.singletons.fresh_ancillas_pool import FreshAncillasPool
-from stim_experiments.utilities import KET_ONE_STATE_VECTOR, KET_ZERO_STATE_VECTOR
 from tests.error_correcting_codes.support.universal_hadamard.single_qubit_code import SingleQubitCode
 from tests.utilities import states_are_equal
-
-
-class UniversalHadamard:
-    def __init__(self, code: ErrorCorrectingCode = None):
-        self._code = code
-
-        configuration = ConfigurationErrorCorrectingCodeManager().get_configuration()
-        self._measurer_type = configuration.measurer_type
-
-    def get_hadamard_circuit(self) -> Circuit:
-        measurement_key = MeasurementKey(f'UNIVERSAL_HADAMARD_MEASUREMENT_{uuid4().hex}')
-        num_qubits_in_desired_encoding = len(self._code.data_qubits)
-        with FreshAncillasPool().use_fresh_ancillas(num_ancillas=3 * num_qubits_in_desired_encoding) as ancilla_qubits:
-            three_cat = ThreeCatCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=ancilla_qubits)
-            uni_h = UniversalHadamardCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=ancilla_qubits)
-            new_encoding = self._code.create_new(qubits=uni_h.subregisters[0])
-            to_computational_logical = UniversalHadamardCodeToComputationalLogical(universal_hadamard_code=uni_h, desired_encoding=self._code)
-            logical_x, logical_z = (list(self._code.get_operation_circuit(operation=LogicalOperation(gate=gate, qubit_index=0)).all_operations())  # allow multi qubit encodings
-                                    for gate in (LogicalGateLabel.X, LogicalGateLabel.Z))
-            return Circuit(
-                three_cat.encode_logical_qubit(),
-                [
-                    ControlledSingleQubitGatesApplier(operations=logical_x, controls=subregister[:len(logical_x)]).get_circuit()
-                    for subregister in three_cat.subregisters
-                ],
-                to_computational_logical.get_circuit(),
-                self._measurer_type(operations=logical_z, measurement_key=measurement_key).get_measurement_circuit(),
-                CircuitOperation(
-                    new_encoding.get_operation_circuit(LogicalOperation(gate=LogicalGateLabel.Z, qubit_index=0)).freeze()
-                ).with_classical_controls(KeyCondition(key=measurement_key)),
-                [
-                    SWAP(new_encoding.data_qubits[i], self._code.data_qubits[i])
-                    for i in range(len(new_encoding.data_qubits))
-                ],
-                [R(ancilla) for ancilla in ancilla_qubits]
-            )
 
 
 class TestUniversalHadamard:
@@ -77,7 +29,8 @@ class TestUniversalHadamard:
         expected_state = (1 / np.sqrt(2)) * array([initial_state[0] + initial_state[1], initial_state[0] - initial_state[1]])
         assert states_are_equal(simulated_state, expected_state)
 
-    def _random_complex_unit_vector(self) -> np.ndarray:
+    @staticmethod
+    def _random_complex_unit_vector() -> np.ndarray:
         dimension = 2
         random_complex_vector = np.random.randn(dimension) + 1j * np.random.randn(dimension)
         unit_complex_vector = random_complex_vector / np.linalg.norm(random_complex_vector)
