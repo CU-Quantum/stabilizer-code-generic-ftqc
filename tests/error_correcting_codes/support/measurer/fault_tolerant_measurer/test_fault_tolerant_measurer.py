1f898820085055d6a6f19f3c505c6f8ad8c2a01b
nikojpapa <npapa1994@gmail.com>
2025-05-16 13:20:46 -0600

    fix ft measurer tests

--- tests/error_correcting_codes/support/measurer/fault_tolerant_measurer/test_fault_tolerant_measurer.py
+++ tests/error_correcting_codes/support/measurer/fault_tolerant_measurer/test_fault_tolerant_measurer.py
@@ -1,91 +1,52 @@
 import numpy.random
-import pytest
-from cirq import Circuit, LineQubit, Simulator, X, \
-    Z
+from cirq import Circuit, LineQubit, MeasurementKey, Simulator, Z
 from numpy.ma.core import allequal
 
+from stim_experiments.error_correcting_codes.error_correcting_code_utilities import get_error_correcting_code_utilities
 from stim_experiments.error_correcting_codes.support.measurer.fault_tolerant_measurer.fault_tolerant_measurer import \
     FaultTolerantMeasurer
-from stim_experiments.utilities import KET_PLUS_STATE_VECTOR, KET_ZERO_STATE_VECTOR, tensor
+from stim_experiments.globals.fresh_ancillas_pool import FreshAncillasPool
+from stim_experiments.utilities import KET_PLUS_STATE_VECTOR
 
 
 class TestFaultTolerantMeasurer:
     def test_trivial(self):
-        measurer = FaultTolerantMeasurer(gates=[], targets=[], measurement_qubit=LineQubit(0), ancillas=[])
+        measurer = FaultTolerantMeasurer(operations=[])
         circuit = measurer.get_measurement_circuit()
         assert circuit == Circuit()
 
     def test_one_qubit_z(self):
-        numpy.random.seed(0)
+        numpy.random.seed(1)
 
-        qubits = LineQubit.range(3)
-        targets = qubits[:1]
-        measurement_qubit = qubits[1]
-        ancillas = qubits[2:]
-        measurement_key = 'TEST'
-        measurer = FaultTolerantMeasurer(gates=[Z],
-                                         targets=targets,
-                                         measurement_qubit=measurement_qubit,
-                                         ancillas=ancillas,
+        qubits = LineQubit.range(1)
+        FreshAncillasPool().set_first_ancilla_num(first_ancilla_num=len(qubits))
+        measurement_key = MeasurementKey('TEST')
+        measurer = FaultTolerantMeasurer(operations=[Z(qubits[0])],
                                          measurement_key=measurement_key)
         circuit = measurer.get_measurement_circuit()
 
         initial_target_state = KET_PLUS_STATE_VECTOR
-        initial_state = tensor(initial_target_state, *[KET_ZERO_STATE_VECTOR] * (len(qubits) - len(targets)))
-        simulator = Simulator()
+        utilities = get_error_correcting_code_utilities(state=initial_target_state)
 
         num_trials = 5
         measurements = []
         for trial in range(num_trials):
-            simulation = simulator.simulate(circuit, qubit_order=qubits, initial_state=initial_state)
-            measurements.extend(simulation.measurements[measurement_key])
+            simulation = utilities.get_state_after_circuit(circuit=circuit,
+                                                           num_data_qubits=len(qubits),
+                                                           initial_data_state=initial_target_state)
+            measurements.extend(simulation.measurements[measurement_key.name])
         assert any(measurements) and not all(measurements)
 
     def test_takes_majority_vote(self):
-        qubits = LineQubit.range(3)
-        targets = qubits[:1]
-        measurement_qubit = qubits[1]
-        ancillas = qubits[2:]
-        measurement_key = 'TEST'
-        measurer = FaultTolerantMeasurer(gates=[Z],
-                                         targets=targets,
-                                         measurement_qubit=measurement_qubit,
-                                         ancillas=ancillas,
+        qubits = LineQubit.range(1)
+        FreshAncillasPool().set_first_ancilla_num(first_ancilla_num=len(qubits))
+        measurement_key = MeasurementKey('TEST')
+        measurer = FaultTolerantMeasurer(operations=[Z(qubits[0])],
                                          measurement_key=measurement_key)
         circuit = measurer.get_measurement_circuit()
 
         simulator = Simulator()
         simulation = simulator.run(circuit)
         assert len(simulation.records) == 2
-        assert allequal(simulation.records[measurement_key], [[[0]]])
-        assert allequal(next(records for key, records in simulation.records.items() if key != measurement_key), [[[0], [0], [0,]]])
-
-    def test_ensure_correct_number_of_ancillas(self):
-        qubits = LineQubit.range(4)
-        measurer = FaultTolerantMeasurer(gates=[X, X], targets=qubits[:2], measurement_qubit=qubits[2], ancillas=qubits[3:])
-        with pytest.raises(ValueError, match="^The number of ancillas \\(1\\) must be at least the number of gates \\(2\\)\\.$"):
-            measurer.get_measurement_circuit()
-
-    def test_ensure_targets_and_measurement_are_disjoint(self):
-        qubits = LineQubit.range(3)
-        measurer = FaultTolerantMeasurer(gates=[X], targets=qubits[:1], measurement_qubit=qubits[0], ancillas=qubits[2:])
-        with pytest.raises(ValueError, match="^The target qubits, measurement qubit, and ancilla qubits must be disjoint\\. "
-                                             "Found duplicate qubit(s) {cirq.LineQubit\\(0\\)\\.$"):
-            measurer.get_measurement_circuit()
-
-    def test_ensure_targets_and_ancilla_are_disjoint(self):
-        qubits = LineQubit.range(3)
-        measurer = FaultTolerantMeasurer(gates=[X], targets=qubits[:1], measurement_qubit=qubits[1], ancillas=qubits[:1])
-        with pytest.raises(ValueError, match="^The target qubits, measurement qubit, and ancilla qubits must be disjoint\\. "
-                                             "Found duplicate qubit\\(s\\) {cirq.LineQubit\\(0\\)}\\.$"):
-            measurer.get_measurement_circuit()
-
-    def test_ensure_measaurement_and_ancilla_are_disjoint(self):
-        qubits = LineQubit.range(3)
-        measurer = FaultTolerantMeasurer(gates=[X], targets=qubits[:1], measurement_qubit=qubits[2],
-                                         ancillas=qubits[2:])
-        with pytest.raises(ValueError,
-                           match="^The target qubits, measurement qubit, and ancilla qubits must be disjoint\\. "
-                                 "Found duplicate qubit\\(s\\) {cirq.LineQubit\\(2\\)}\\.$"):
-            measurer.get_measurement_circuit()
-
+        assert allequal(simulation.records[measurement_key.name], [[[0]]])
+        assert allequal(next(records for key, records in simulation.records.items() if key != measurement_key.name), [[[0], [0], [0,]]])
