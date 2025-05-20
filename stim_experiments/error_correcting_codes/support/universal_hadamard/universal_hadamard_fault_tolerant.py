0d2f5c727c2d9493efb7f69504c3229c2326406c
nikojpapa <npapa1994@gmail.com>
2025-05-20 17:37:46 -0600

    uni h with only three times qubits remove extra ancillas

--- stim_experiments/error_correcting_codes/support/universal_hadamard/universal_hadamard_fault_tolerant.py
+++ stim_experiments/error_correcting_codes/support/universal_hadamard/universal_hadamard_fault_tolerant.py
@@ -6,11 +6,9 @@ from uuid import uuid4
 import sympy
 from cirq import Circuit, CircuitOperation, FrozenCircuit, LineQubit, MeasurementKey, OP_TREE, Operation, \
     R, X
-from sympy.physics.quantum import gate
 
 from stim_experiments.custom_dataclasses.configuration_error_correcing_code import ConfigurationErrorCorrectingCode
 from stim_experiments.custom_dataclasses.logical_operation import LogicalGateLabel, LogicalOperation
-from stim_experiments.custom_dataclasses.state_encoding import StateEncoding
 from stim_experiments.error_correcting_codes.repetition_code.repetition_code import RepetitionCode
 from stim_experiments.error_correcting_codes.support.cat_state_creator.cat_state_creator import CatStateCreator
 from stim_experiments.error_correcting_codes.support.controlled_single_qubit_gates_applier import \
@@ -31,12 +29,10 @@ from stim_experiments.utilities.utilities import cx_sequentially_further_qubits_
 @dataclass
 class UniversalHadamardFaultTolerantContext:
     ancilla_qubits: list[LineQubit]
-    three_subregister_parity_codes_small: list[ThreeSubregisterParityCode]
-    three_subregister_parity_code_large: ThreeSubregisterParityCode
-    three_cat_small: ThreeCatCode
-    three_cat_large: ThreeCatCode
     data_code_logical_x: list[Operation]
     data_code_logical_z: list[Operation]
+    three_subregister_parity_code: ThreeSubregisterParityCode
+    three_cat: ThreeCatCode
     universal_hadamard_helper_code: UniversalHadamardHelperCode
 
 
@@ -48,25 +44,21 @@ class UniversalHadamardFaultTolerant(UniversalHadamard):
                 self._cxz_helpers_to_data(context=context),
                 self._ensure_subregister_parity_in_plus(context=context),
 
-                # self._encode_helper_registers(context=context),
-                # self._cx_original_to_helpers(context=context),
-                # self._create_cat_states_on_all_subregisters(context=context),
-
                 self._encode_universal_hadamard_helper(context=context),
                 self._measure_out_helper(context=context),
                 self._reset_ancilla_qubits(context=context),
             )
 
     def _encode_three_cat(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
-        with ActiveEncodingsStore(additional_tracked_encodings=[context.three_cat_small]) as encodings_store:
+        with ActiveEncodingsStore(additional_tracked_encodings=[context.three_cat]) as encodings_store:
             return [
-                context.three_cat_small.encode_logical_qubit(),
+                context.three_cat.encode_logical_qubit(),
                 encodings_store.get_all_correction_circuits()
             ]
 
     def _cxz_helpers_to_data(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
         repetition_codes = [RepetitionCode(num_qubits=len(subregister), qubits=subregister)
-                            for subregister in context.three_cat_small.subregisters]
+                            for subregister in context.three_cat.subregisters]
         with ActiveEncodingsStore(additional_tracked_encodings=repetition_codes) as encodings_store:
             return [
                 self._c_operations_helpers_to_data(operations=context.data_code_logical_x, encodings_store=encodings_store,  context=context),
@@ -82,51 +74,19 @@ class UniversalHadamardFaultTolerant(UniversalHadamard):
                 ControlledSingleQubitGatesApplier(operations=operations, controls=subregister[:len(operations)]).get_circuit(),
                 encodings_store.get_all_correction_circuits(),
             ]
-            for subregister in context.three_cat_small.subregisters
+            for subregister in context.three_cat.subregisters
         ]
-
-    def _encode_helper_registers(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
-        additional_universal_hadamard_codes = context.three_subregister_parity_codes_small[1:]
-        with ActiveEncodingsStore(additional_tracked_encodings=additional_universal_hadamard_codes) as encodings_store:
-            return [
-                [code.encode_logical_qubit() for code in additional_universal_hadamard_codes],
-                encodings_store.get_all_correction_circuits(),
-            ]
-
-    def _cx_original_to_helpers(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
-        with ActiveEncodingsStore(additional_tracked_encodings=context.three_subregister_parity_codes_small) as encodings_store:
-            return [
-                [
-                    [
-                        X(target_qubit).controlled_by(control_qubit)
-                        for target_qubit, control_qubit in zip(code.data_qubits, context.three_subregister_parity_codes_small[0].data_qubits)
-                    ],
-                    encodings_store.get_all_correction_circuits(),
-                ] for code in context.three_subregister_parity_codes_small[1:]
-            ]
-
-    def _create_cat_states_on_all_subregisters(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
-        with ActiveEncodingsStore(additional_tracked_encodings=[context.three_cat_large]) as encodings_store:
-            return [
-                [
-                    [
-                        cx_sequentially_further_qubits_from_first(qubits=subregister),
-                        self._cat_state_creator_type(qubit_register=subregister).get_cat_state_circuit(),
-                    ] for code in context.three_subregister_parity_codes_small for subregister in code.subregisters
-                ],
-                encodings_store.get_all_correction_circuits(),
-            ]
 
     def _ensure_subregister_parity_in_plus(self, context: UniversalHadamardFaultTolerantContext) -> OP_TREE:
         measurement_key = MeasurementKey(f'PREPARE_SUBREGISTER_PARITY_CODE_{uuid4().hex}')
         measurement_symbol = sympy.symbols(measurement_key.name)
         subregister_parity_x, subregister_parity_z = (
-            list(context.three_subregister_parity_codes_small[0].get_operation_circuit(
+            list(context.three_subregister_parity_code.get_operation_circuit(
                 operation=LogicalOperation(gate=gate, qubit_index=0)
             ).all_operations())
             for gate in (LogicalGateLabel.X, LogicalGateLabel.Z)
         )
-        with ActiveEncodingsStore(additional_tracked_encodings=[context.three_subregister_parity_codes_small[0]]) as encodings_store:
+        with ActiveEncodingsStore(additional_tracked_encodings=[context.three_subregister_parity_code]) as encodings_store:
             return [
                 self._measurer_type(
                     operations=subregister_parity_x + subregister_parity_z + context.data_code_logical_x + context.data_code_logical_z,
@@ -139,7 +99,7 @@ class UniversalHadamardFaultTolerant(UniversalHadamard):
             ]
 
     def _encode_universal_hadamard_helper(self, context: UniversalHadamardFaultTolerantContext):
-        subregister_to_cat = context.three_subregister_parity_codes_small[0].subregisters[0]
+        subregister_to_cat = context.three_subregister_parity_code.subregisters[0]
         with ActiveEncodingsStore(additional_tracked_encodings=[context.universal_hadamard_helper_code]) as encodings_store:
             return [
                 [
@@ -171,20 +131,16 @@ class UniversalHadamardFaultTolerant(UniversalHadamard):
 
     @contextmanager
     def _use_fresh_ancilla_qubits(self) -> Generator[UniversalHadamardFaultTolerantContext, None, None]:
-        num_subregister_parity_codes = 3
         num_qubits_in_desired_encoding = len(self._code.data_qubits)
         num_qubits_per_subregister_parity_code = num_qubits_in_desired_encoding * ThreeSubregisterParityCode.num_cats
-        with FreshAncillasPool().use_fresh_ancillas(num_ancillas=num_subregister_parity_codes * num_qubits_per_subregister_parity_code) as ancilla_qubits:
-            three_subregister_parity_codes = [
-                ThreeSubregisterParityCode(
-                    num_qubits_in_cat_state=num_qubits_in_desired_encoding,
-                    qubits=ancilla_qubits[code_index * num_qubits_per_subregister_parity_code:(code_index + 1) * num_qubits_per_subregister_parity_code],
-                )
-                for code_index in range(num_subregister_parity_codes)
-            ]
+        with FreshAncillasPool().use_fresh_ancillas(num_ancillas=num_qubits_per_subregister_parity_code) as ancilla_qubits:
+            three_subregister_parity_code = ThreeSubregisterParityCode(
+                num_qubits_in_cat_state=num_qubits_in_desired_encoding,
+                qubits=ancilla_qubits[:num_qubits_per_subregister_parity_code],
+            )
             universal_hadamard_helper_code = UniversalHadamardHelperCode(
-                num_qubits_in_cat_state=len(three_subregister_parity_codes[0].subregisters[0]),
-                qubits=three_subregister_parity_codes[0].data_qubits
+                num_qubits_in_cat_state=len(three_subregister_parity_code.subregisters[0]),
+                qubits=three_subregister_parity_code.data_qubits
             )
             logical_x, logical_z = (
                 list(self._code.get_operation_circuit(
@@ -194,12 +150,10 @@ class UniversalHadamardFaultTolerant(UniversalHadamard):
             )
             yield UniversalHadamardFaultTolerantContext(
                 ancilla_qubits=ancilla_qubits,
-                three_subregister_parity_codes_small=three_subregister_parity_codes,
-                three_subregister_parity_code_large=ThreeSubregisterParityCode(num_qubits_in_cat_state=len(ancilla_qubits) // num_subregister_parity_codes, qubits=ancilla_qubits),
-                three_cat_small=ThreeCatCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=three_subregister_parity_codes[0].data_qubits),
-                three_cat_large=ThreeCatCode(num_qubits_in_cat_state=len(ancilla_qubits) // num_subregister_parity_codes, qubits=ancilla_qubits),
                 data_code_logical_x=logical_x,
                 data_code_logical_z=logical_z,
+                three_subregister_parity_code=three_subregister_parity_code,
+                three_cat=ThreeCatCode(num_qubits_in_cat_state=num_qubits_in_desired_encoding, qubits=three_subregister_parity_code.data_qubits),
                 universal_hadamard_helper_code=universal_hadamard_helper_code
             )
 
