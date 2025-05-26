620e7b4bc2e9472381496b76ae37ae4fdc6b38fe
nikojpapa <npapa1994@gmail.com>
2025-05-25 23:05:30 -0600

    anticommuters

--- stim_experiments/error_correcting_codes/tetrahedral_code/tetrahedral_code.py
+++ stim_experiments/error_correcting_codes/tetrahedral_code/tetrahedral_code.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from cirq import Circuit, LineQubit, Operation
+from cirq import Circuit, LineQubit, Operation, X, Z
 
 from stim_experiments.custom_dataclasses.check_matrix import CheckMatrix
 from stim_experiments.custom_dataclasses.logical_operation import LogicalOperation
@@ -14,7 +14,22 @@ class TetrahedralCode(StabilizerCode):
         super().__init__(check_matrix=check_matrix, qubits=qubits)
 
     def _get_anticommuter_for_generator(self, generator_index: int) -> list[Operation]:
-        pass
+        return [
+            [X(self.data_qubits[0])],
+            [X(self.data_qubits[1])],
+            [X(self.data_qubits[3])],
+            [X(self.data_qubits[7])],
+            [X(self.data_qubits[i]) for i in range(3)],
+            [X(self.data_qubits[i]) for i in (0, 3, 4)],
+            [X(self.data_qubits[i]) for i in (1, 3, 5)],
+            [X(self.data_qubits[i]) for i in (1, 7, 9)],
+            [X(self.data_qubits[i]) for i in (3, 7, 11)],
+            [X(self.data_qubits[i]) for i in (0, 7, 8)],
+            [Z(self.data_qubits[0])],
+            [Z(self.data_qubits[1])],
+            [Z(self.data_qubits[3])],
+            [Z(self.data_qubits[7])],
+        ][generator_index]
 
     def _perform_get_operation_circuit(self, operation: LogicalOperation) -> Optional[Circuit]:
         pass
