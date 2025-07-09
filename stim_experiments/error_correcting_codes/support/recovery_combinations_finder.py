40cbffaf6fc1696f95c25574754dd481288ef7c0
nikojpapa <npapa1994@gmail.com>
2025-07-09 17:35:52 -0600

    recovery combos different x and z

--- stim_experiments/error_correcting_codes/support/recovery_combinations_finder.py
+++ stim_experiments/error_correcting_codes/support/recovery_combinations_finder.py
@@ -1,23 +1,47 @@
 from collections import defaultdict
 from dataclasses import replace
 from itertools import combinations
+from typing import Optional
 
 import numpy as np
+from cirq import X, Y, Z
 
 from stim_experiments.custom_dataclasses.recovery import RecoveryGate, RecoveryOperation
 
 
 class RecoveryCombinationsFinder:
-    def __init__(self, max_num_errors: int):
+    def __init__(self, max_num_errors: Optional[int] = None, max_num_x_errors: Optional[int] = None, max_num_z_errors: Optional[int] = None):
         self._max_num_errors = max_num_errors
+        self._max_num_x_errors = max_num_x_errors
+        self._max_num_z_errors = max_num_z_errors
 
     def find_recoveries(self, single_error_recoveries: list[RecoveryGate | RecoveryOperation]) -> list[RecoveryGate | RecoveryOperation]:
+        self._validate()
         recovery_gates = []
         for num_errors in range(1, self._max_num_errors + 1):
             combos = combinations(single_error_recoveries, num_errors)
             for combo in combos:
                 symptoms = [recovery_gate.symptom for recovery_gate in combo]
-                symptoms_combined = np.sum(symptoms, axis=0).tolist()
-                for recovery_gate in combo:
-                    recovery_gates.append(replace(recovery_gate, symptom=symptoms_combined))
+                symptoms_combined = np.mod(np.sum(symptoms, axis=0), 2).tolist()
+
+                error_types = [recovery_gate.gate for recovery_gate in combo]
+                y_errors = error_types.count(Y)
+                x_is_below_limit = error_types.count(X) + y_errors <= self._max_num_x_errors
+                z_is_below_limit = error_types.count(Z) + y_errors <= self._max_num_z_errors
+                if x_is_below_limit and z_is_below_limit:
+                    for recovery_gate in combo:
+                        recovery_gates.append(replace(recovery_gate, symptom=symptoms_combined))
         return recovery_gates
+
+    def _validate(self) -> None:  # TODO test this
+        if self._max_num_errors is None and (self._max_num_x_errors is None or self._max_num_z_errors is None):
+            raise ValueError(
+                'Either "max_num_errors" must be provided or both "max_num_x_errors" and "max_num_z_errors" must be provided.'
+            )
+
+        if self._max_num_errors is None:
+            self._max_num_errors = self._max_num_x_errors + self._max_num_z_errors
+        if self._max_num_x_errors is None:
+            self._max_num_x_errors = self._max_num_errors
+        if self._max_num_z_errors is None:
+            self._max_num_z_errors = self._max_num_errors
--- tests/error_correcting_codes/support/test_recovery_combinations_finder.py
+++ tests/error_correcting_codes/support/test_recovery_combinations_finder.py
@@ -1,4 +1,4 @@
-from cirq import LineQubit, X, Z
+from cirq import LineQubit, X, Y, Z
 from numpy import array
 
 from stim_experiments.custom_dataclasses.check_matrix import CheckMatrix
@@ -75,5 +75,184 @@ class TestRecoveryCombinationsFinder:
             RecoveryOperation(
                 operation=X(qubits[1]),
                 symptom=[1, 1]
+            ),
+        ]
+
+    def test_different_max_x_z(self):
+        check_matrix = CheckMatrix(matrix=array([
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+        ]))
+        recoveries = RecoveryFinder(check_matrix=check_matrix).find_recovery_gates()
+        combos = RecoveryCombinationsFinder(max_num_x_errors=2, max_num_z_errors=1).find_recoveries(single_error_recoveries=recoveries)
+        assert combos == [
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 0, 0, 0]
+            ),
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 1, 0, 0]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[0, 0, 1, 0]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[0, 0, 0, 1]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 0, 1, 0]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[1, 0, 1, 0]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 0, 0, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[1, 0, 0, 1]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 1, 1, 0]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[0, 1, 1, 0]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 1, 0, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[0, 1, 0, 1]
+            ),
+
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[0, 0, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[0, 0, 1, 1]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 0, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[1, 0, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[1, 0, 1, 1]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 1, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=2,
+                symptom=[0, 1, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=3,
+                symptom=[0, 1, 1, 1]
+            ),
+        ]
+
+    def test_different_max_x_z_with_y_errors(self):
+        check_matrix = CheckMatrix(matrix=array([[1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 0]]))
+        recoveries = RecoveryFinder(check_matrix=check_matrix).find_recovery_gates()
+        combos = RecoveryCombinationsFinder(max_num_x_errors=2, max_num_z_errors=1).find_recoveries(single_error_recoveries=recoveries)
+        assert combos == [
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 0, 0]
+            ),
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 0, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=0,
+                symptom=[0, 1, 0]
+            ),
+            RecoveryGate(
+                gate=Y,
+                qubit_index=0,
+                symptom=[1, 1, 0]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=0,
+                symptom=[1, 1, 0]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=0,
+                symptom=[1, 1, 0]
+            ),
+
+            RecoveryGate(
+                gate=Z,
+                qubit_index=1,
+                symptom=[0, 1, 1]
+            ),
+            RecoveryGate(
+                gate=X,
+                qubit_index=0,
+                symptom=[0, 1, 1]
+            ),
+
+            RecoveryGate(
+                gate=X,
+                qubit_index=0,
+                symptom=[1, 0, 0]
+            ),
+            RecoveryGate(
+                gate=Y,
+                qubit_index=0,
+                symptom=[1, 0, 0]
             ),
         ]
