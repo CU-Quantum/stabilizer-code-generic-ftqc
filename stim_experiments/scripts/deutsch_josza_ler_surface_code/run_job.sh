a696d9c7dd53f0cd31e4027378f1e868f05c2f46
nikojpapa <npapa1994@gmail.com>
2025-07-17 11:02:57 -0600

    setup workspace

--- stim_experiments/scripts/deutsch_josza_ler_surface_code/run_job.sh
+++ stim_experiments/scripts/deutsch_josza_ler_surface_code/run_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=aa100
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:16
+#SBATCH --gres=gpu:3
 #SBATCH --ntasks=1
 #SBATCH --qos=normal
 #SBATCH --job-name=deutsch_josza
@@ -19,8 +19,14 @@ module load anaconda/2023.09
 conda env create -f environment.yml
 
 
-# Run your scripts
+# Setup workspace
+export PYTHONPATH=$PYTHONPATH:~/workspace/stim-experiments/stim-experiments
+mkdir -p ~/workspace/stim-experiments/results/deutsch_josza
+cd ~/workspace/stim-experiments/results/deutsch_josza
+
+
+# Run using cuQuantum Appliance
 export image_name=nvcr.io/nvidia/cuquantum-appliance:25.03-x86_64
 apptainer pull ${image_name}.sif docker://${image_name}
 apptainer run --gpus all --rm ${image_name} \
-  python ~/workspace/stim-experiments/scripts/deutsch_josza_ler_surface_code/deutsch_josza_ler_surface_code.py
+  python ~/workspace/stim-experiments/stim-experiments/scripts/deutsch_josza_ler_surface_code/deutsch_josza_ler_surface_code.py
