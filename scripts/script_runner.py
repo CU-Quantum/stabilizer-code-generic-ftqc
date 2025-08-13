655ec24cd0b8ef4b70932e68f0dbdcf8406340d5
nikojpapa <npapa1994@gmail.com>
2025-08-12 19:14:48 -0600

    fix percentage

--- scripts/script_runner.py
+++ scripts/script_runner.py
@@ -77,7 +77,8 @@ class ScriptRunner:
             results.append(result)
         measurements_per_shot = [result.measurements_per_shot[0] for result in results]
         sum_measurements_per_shot = self._was_successful_func(measurements_per_shot)
-        num_successful_shots = np.count_nonzero(sum_measurements_per_shot)
+        num_assumed_success = self._num_shots - len(self._circuits_noisy)
+        num_successful_shots = np.count_nonzero(sum_measurements_per_shot) + num_assumed_success
         print()
         print(f"----MEASUREMENTS PER SHOT----: {measurements_per_shot}")
 
