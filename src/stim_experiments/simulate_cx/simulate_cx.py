7e2940ecd576dd6be61b79808b34acf64c017f27
nikojpapa <npapa1994@gmail.com>
2025-10-25 15:34:13 -0600

    fix si observables

--- src/stim_experiments/simulate_cx/simulate_cx.py
+++ src/stim_experiments/simulate_cx/simulate_cx.py
@@ -80,7 +80,7 @@ class SimulateCx:
         observable[:len(self._target_code_utilities.z_observable) // 2] = self._target_code_utilities.z_observable[:len(self._target_code_utilities.z_observable) // 2]
         observable[len(observable) // 2:len(observable) // 2 + len(self._target_code_utilities.z_observable) // 2] = self._target_code_utilities.z_observable[len(self._target_code_utilities.z_observable) // 2:]
         # control observable
-        observable[[len(observable) // 2 + len(self._target_code_utilities.z_observable) // 2 + i for i in range(self._si + 1)]] = np.ones(self._si + 1)
+        observable[[len(observable) // 2 + len(self._target_code_utilities.z_observable) // 2 + i * self._num_qubits_per_cat_state for i in range(self._si + 1)]] = np.ones(self._si + 1)
 
         return combined_symplectic_matrix, observable
 
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     # target_code = get_15_1_3_reed_solomon_code_utilities()
     # target_code = get_shor_code_utilities(num_cat_states=3, num_qubits_per_cat_state=3, z_observable=get_shor_h_observable_z(distance=3), x_observable=get_shor_h_observable_x(distance=3))
     target_code = get_five_qubit_code_utilities()
-    samples = SimulateCx(num_cat_states=3, target_code_utilities=target_code, si=0).run_main()
+    samples = SimulateCx(num_cat_states=3, target_code_utilities=target_code, si=1).run_main()
 
     # Print samples as CSV data.
     print(CSV_HEADER)
