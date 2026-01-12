27f4c653de01bb123aae7ed2e179beebf21ff598
nikojpapa <npapa1994@gmail.com>
2026-01-12 14:53:18 -0700

    measure out universal t

--- src/cirq_experiments/support/universal_operations/universal_t/universal_t_fault_tolerant.py
+++ src/cirq_experiments/support/universal_operations/universal_t/universal_t_fault_tolerant.py
@@ -1,18 +1,27 @@
 from contextlib import contextmanager
 from dataclasses import dataclass
 from typing import Generator
+from uuid import uuid4
 
-from cirq import Circuit, LineQubit, Moment, OP_TREE, R
+from cirq import Circuit, CircuitOperation, LineQubit, FrozenCircuit, Moment, OP_TREE, R, TaggedOperation
+import sympy
 
+from cirq_experiments.custom_dataclasses.configuration_error_correcing_code import ConfigurationErrorCorrectingCode
 from cirq_experiments.custom_dataclasses.logical_operation import LogicalGateLabel, LogicalOperation
 from cirq_experiments.custom_dataclasses.simulation_operation import TargetEncoding
 from cirq_experiments.support.universal_operations.universal_controlled_flip.universal_controlled_flip import \
     UniversalControlledOperation
+from cirq_experiments.support.measurer.measurer import Measurer
+from cirq_experiments.support.operations_applier.operations_applier import DELAYED_NOISE_TAG
 from cirq_experiments.support.universal_operations.universal_t.universal_t import UniversalT
 from cirq_experiments.error_correcting_codes.tetrahedral_code.tetrahedral_code import TetrahedralCode
 from cirq_experiments.globals.active_encodings_store import ActiveEncodingsStore
 from cirq_experiments.globals.error_correcting_code_configuration import ConfigurationErrorCorrectingCodeManager
 from cirq_experiments.globals.fresh_ancillas_pool import FreshAncillasPool
+from cirq_experiments.utilities.measurement_key_with_stable_hash import MeasurementKeyWithStableHash
+
+
+UNIVERSAL_T_MEASUREMENT_TAG = 'UNIVERSAL_T_MEASUREMENT_TAG'
 
 
 @dataclass
@@ -27,9 +36,9 @@ class UniversalTFaultTolerant(UniversalT):
             return Circuit(
                 Moment(self._reset_ancilla_qubits(context=context)),
                 self._encode_tetrahedral(context=context),
-                self._cx_code_to_tetrahedral(context=context, perform_correction_round=True),
+                self._cx_code_to_tetrahedral(context=context),
                 self._perform_t_on_tetrahedral(context=context),
-                self._cx_code_to_tetrahedral(context=context, perform_correction_round=False),
+                self._measure_out_helper(context=context),
             )
 
     def _encode_tetrahedral(self, context: UniversalTFaultTolerantContext) -> OP_TREE:
@@ -39,13 +48,13 @@ class UniversalTFaultTolerant(UniversalT):
                 encodings_store.get_all_correction_circuits(),
             ]
 
-    def _cx_code_to_tetrahedral(self, context: UniversalTFaultTolerantContext, perform_correction_round: bool) -> OP_TREE:
+    def _cx_code_to_tetrahedral(self, context: UniversalTFaultTolerantContext) -> OP_TREE:
         target_encoding = TargetEncoding(operation=LogicalOperation(gate=LogicalGateLabel.X, qubit_index=0),
                                          encoding=context.tetrahedral)
         with ActiveEncodingsStore(additional_tracked_encodings=[context.tetrahedral]) as encodings_store:
             return [
                 self._universal_controlled_operation_type(control=self._encoding, target=target_encoding).get_controlled_operation_circuit(),
-                encodings_store.get_all_correction_circuits() if perform_correction_round else [],
+                encodings_store.get_all_correction_circuits(),
             ]
 
     def _perform_t_on_tetrahedral(self, context: UniversalTFaultTolerantContext) -> OP_TREE:
@@ -54,6 +63,36 @@ class UniversalTFaultTolerant(UniversalT):
                 context.tetrahedral.get_operation_circuit(LogicalOperation(gate=LogicalGateLabel.T, qubit_index=0)),
                 encodings_store.get_all_correction_circuits(),
             ]
+
+    def _measure_out_helper(self, context: UniversalTFaultTolerantContext) -> OP_TREE:
+        measurement_key = MeasurementKeyWithStableHash(f'UNIVERSAL_T_MEASUREMENT_{uuid4().hex}')
+        measurement_key_symbol = sympy.symbols(measurement_key.name)
+        helper_code_logical_x = list(context.tetrahedral.get_operation_circuit(
+            operation=LogicalOperation(gate=LogicalGateLabel.X, qubit_index=0)
+        ).all_operations())
+        data_code_logical_z = list(self._encoding.encoding.get_operation_circuit(
+            operation=LogicalOperation(gate=LogicalGateLabel.Z, qubit_index=self._encoding.qubit_index_relative)
+        ).all_operations())
+        with ActiveEncodingsStore(additional_tracked_encodings=[]) as encodings_store:
+            return TaggedOperation(
+                CircuitOperation(
+                    FrozenCircuit(
+                        self._measurer_type(observables=[helper_code_logical_x], measurement_keys=[measurement_key]).get_measurement_circuit(),
+                        encodings_store.get_all_correction_circuits(),
+                        FrozenCircuit(TaggedOperation(  # FrozenCircuit to keep separate from correction round
+                            CircuitOperation(
+                                FrozenCircuit(
+                                    CircuitOperation(
+                                        FrozenCircuit(data_code_logical_z)).with_classical_controls(
+                                        measurement_key),
+                                )
+                            ),
+                            DELAYED_NOISE_TAG
+                        )),
+                    )
+                ),
+                UNIVERSAL_T_MEASUREMENT_TAG
+            )
 
     def _reset_ancilla_qubits(self, context: UniversalTFaultTolerantContext) -> OP_TREE:
         return [R(qubit) for qubit in context.ancilla_qubits]
@@ -70,3 +109,11 @@ class UniversalTFaultTolerant(UniversalT):
     @property
     def _universal_controlled_operation_type(self) -> type[UniversalControlledOperation]:
         return ConfigurationErrorCorrectingCodeManager().get_configuration().universal_controlled_operation_type
+
+    @property
+    def _measurer_type(self) -> type[Measurer]:
+        return self._configuration.measurer_type
+
+    @property
+    def _configuration(self) -> ConfigurationErrorCorrectingCode:
+        return ConfigurationErrorCorrectingCodeManager().get_configuration()
