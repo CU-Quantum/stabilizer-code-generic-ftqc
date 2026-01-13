9f590e5e29fd1be4009f1a99884b70af741c863c
nikojpapa <nipa4599@colorado.edu>
2026-01-13 14:02:21 -0700

    merge progress

---
+++ src/stim_experiments/scripts/run_merge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --account=ucb685_asc1
+#SBATCH --time=24:00:00
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=merge
+#SBATCH --error=results/merge/merge_%j.err
+#SBATCH --output=results/merge/merge_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nipa4599@colorado.edu
+
+
+# Create and activate a virtual environment
+module load anaconda/2023.09
+conda env create -f /projects/nipa4599/stim-experiments/environment.yml
+conda activate stim-experiments-venv
+
+
+# Setup workspace
+export PYTHONPATH=$PYTHONPATH:/projects/nipa4599/stim-experiments/src
+cd /projects/nipa4599/stim-experiments || exit
+
+python /projects/nipa4599/stim-experiments/src/stim_experiments/scripts/merge_results.py
+
