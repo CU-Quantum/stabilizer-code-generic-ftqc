a7513b9b73c9e144d309776ea45339b6a234104a
nikojpapa <npapa1994@gmail.com>
2026-07-28 17:44:08 -0600

    golay params

--- src/stim_experiments/scripts/golay/run_job.sh
+++ src/stim_experiments/scripts/golay/run_job.sh
@@ -30,5 +30,5 @@ cd /projects/nipa4599/stim-experiments || exit
 python /projects/nipa4599/stim-experiments/src/stim_experiments/scripts/golay/golay.py \
   -s 1_000_000_000 \
   -e 1000 \
-  -p 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1 \
-  -d bposd
+  -p 5e-5 1e-4 2e-4 5e-4 1e-3 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1 \
+  -d exact_mw
