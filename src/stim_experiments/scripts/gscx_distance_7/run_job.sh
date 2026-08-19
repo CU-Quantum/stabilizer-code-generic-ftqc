ece502cd7242e0a22337e2c6ba49bdcf9b7f8ef7
nikojpapa <nipa4599@colorado.edu>
2026-08-19 11:31:19 -0600

    progress

--- src/stim_experiments/scripts/gscx_distance_7/run_job.sh
+++ src/stim_experiments/scripts/gscx_distance_7/run_job.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --account=ucb685_asc2
 #SBATCH --time=24:00:00
-#SBATCH --array=0-9
+#SBATCH --array=0-498
 #SBATCH --partition=amem
 #SBATCH --qos=mem-normal
 #SBATCH --mem=512G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=gsc_d7
+#SBATCH --job-name=gscx_d7
 #SBATCH --error=results/gscx_distance_7/gscx_distance_7_%j.err
 #SBATCH --output=results/gscx_distance_7/gscx_distance_7_%j.out
 #SBATCH --mail-type=ALL
@@ -28,6 +28,7 @@ cd /projects/nipa4599/stim-experiments || exit
 
 # Run
 python /projects/nipa4599/stim-experiments/src/stim_experiments/scripts/gscx_distance_7/gscx_distance_7.py \
-  -s 100_000_000 \
-  -e 500 \
+  -s 1_000_000_000 \
+  -e 1000 \
   -p 2e-4 5e-4 1e-3 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1
+
